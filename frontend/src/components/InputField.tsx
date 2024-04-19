3c183d123b795e99b9dd754a3f1482518147b769
Leigh Michael Forrest <leigh@catbirdseat.io>
2024-04-19 18:02:17 -0400

    further cleanup of InputField

--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -1,19 +1,15 @@
-import {  forwardRef } from 'react'
-import { IInputProps } from '../@types/form';
-import FieldError from './FieldError';
+import { forwardRef } from "react";
+import { IInputProps } from "../@types/form";
+import FieldError from "./FieldError";
 
+const InputField = forwardRef<HTMLInputElement, IInputProps>(
+  ({ label, type = "text", ...props }, ref) => (
+    <div>
+      <label>{label}</label>
+      <input type={type} ref={ref} {...props} />
+      <FieldError name={props.name} />
+    </div>
+  )
+);
 
-const InputField = forwardRef<HTMLInputElement, IInputProps>(function Input(
-    { label, type = "text", ...props },
-    ref
-  ) {
-    return (
-      <div>
-        <label>{label}</label>
-        <input type={type} ref={ref} {...props} />
-        <FieldError name={props.name} />
-      </div>
-    );
-  });
-
-export default InputField
+export default InputField;
